48440dc8d03877ff611d82a03ef4ab27378e533b
Siddhant Jain <siddhantjain209@gmail.com>
2022-11-26 18:52:29 +0530

    add .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-rflsw(r3j3na%#h=n)4pu80*#*3%=d-fx^7@n5=$6z2l-zyf'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'artzy',
-        'USER': 'root',
-        'PASSWORD': 'whoAMI@123',
+        'NAME': config('DB_NAME'),
+        'USER': config('MYSQL_SERVER_USERNAME'),
+        'PASSWORD': config('MYSQL_SERVER_PASSWORD'),
         'HOST':'localhost',
         'PORT':'3306',
     }
